e6ff277e2114accfd22d93a60d1925a88813154b
RozenX1 <raul.campos@alu.uclm.es>
2015-12-21 00:45:06 +0100

    Datastore completed.

--- app_types.py
+++ app_types.py
@@ -4,25 +4,55 @@
 from google.appengine.ext import ndb
 
 class Client(ndb.Model):
-	email = ndb.KeyProperty(kind=str)
+	# email = ndb.StringProperty() 
 	password = ndb.StringProperty(required=True)
 	address = ndb.StringProperty(required=True)
 	phone = ndb.IntegerProperty(required=True)
 
-class Cart(ndb.Model):
-	name = ndb.StringProperty()
-	wines = ndb.StringProperty(repeated=True)
+	def json(self):
+		return {
+			"url":self.key.urlsafe(),
+			"email":self.key.id(),
+			"password":self.password,
+			"address":self.address,
+			"phone":self.phone
+		}
 
 class Wine(ndb.Model):
 	grade = ndb.FloatProperty(default=12.0)
 	size = ndb.IntegerProperty(default=75)
 	varietals = ndb.StringProperty(repeated=True)
-	do = BooleanProperty(default=False)
-	price = FloatProperty()
-	name = StringProperty(required=True)
-	photo = BlobKeyProperty()
-	cask = IntegerProperty()
-	bottle = IntegerProperty()
+	do = ndb.BooleanProperty(default=False)
+	price = ndb.FloatProperty()
+	name = ndb.StringProperty(required=True)
+	photo = ndb.BlobKeyProperty()
+	cask = ndb.IntegerProperty()
+	bottle = ndb.IntegerProperty()
+
+	def json(self):
+		return {
+			"url":self.key.urlsafe(),
+			"grade":self.grade,
+			"size":self.size,
+			"varietals":self.varietals,
+			"do":self.do,
+			"price":self.price,
+			"name":self.name,
+			"photo":self.photo,
+			"cask":self.cask,
+			"bottle":self.bottle
+		}
+
+class Cart(ndb.Model):
+	name = ndb.StringProperty()
+	items = ndb.KeyProperty(repeated=True, kind=Wine)
+
+	def json(self):
+		return {
+			"name":self.name,
+			"items":self.items
+		}
 
 class WineType(ndb.Model):
-	kind = ndb.KeyProperty(kind=str)
+	pass
+
--- wines.py
+++ wines.py
@@ -1,266 +1,194 @@
 #!/usr/bin/python
 # -*- coding:utf-8; tab-width:4; mode:python -*-
 
-from base64 import b64encode
-from uuid import uuid4 
 from flask import Flask, jsonify, abort, make_response, request, url_for
+from google.appengine.ext import ndb
 from app_types import *
+from functools import wraps
+from itertools import dropwhile
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-grp_wines = {"red":{}, "pink":{}, "white":{}}
-all_wines = {}
-clients = {}
-carts = {}
+#Create wine types
+for t in ("red", "white", "pink"):
+	new_type = WineType(id=t)
+	new_type.put()
+
+# AUXILIARY
+# ------------------------------------------------------------------------
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not Found'}), 404)
+    return make_response(jsonify({'Error': 'Not Found'}), 404)
+
+def retrieve(ID, cls=None):
+	try:
+		if cls:
+			key = ndb.Key(cls, ID)
+		else:
+			key = ndb.Key(urlsafe=ID)
+	except:
+		abort(404)
+	return key
 
 def check_vals(vals):
-	if not all(x in request.form.keys() for x in vals):
-		abort(400) # Bad request
+	'''It checks whether the values are present in the request.'''
+	def inner(action):
+		@wraps(action)
+		def dec(**kwargs):
+			keys = request.get_json().keys()
+			if not all(x in keys for x in vals): abort(400)
+			return action(**kwargs)
+		return dec
+	return inner
 
-# newClient() - POST
+def check_cart(action):
+	'''It checks whether the cart corresponds with the client.'''
+	@wraps(action)
+	def dec(**kwargs):
+		kwargs["key_client"] = retrieve(kwargs.pop("ID_client"))
+		kwargs["key_cart"] = retrieve(kwargs.pop("ID_cart"))
+		if not kwargs["key_cart"].parent() == kwargs["key_client"]: abort(404)
+		return action(**kwargs)
+	return dec
+
+# ACTIONS
+# ------------------------------------------------------------------------
+
 @app.route('/clients', methods=['POST'])
+@check_vals(["email", "password", "address", "phone"])
 def newClient():
-	# Check whether values are present in the request
-	check_vals(["email", "pass", "address", "phone"])
-	# Check whether client is not repeated
-	new_client = Client(
-		email=request.form["email"],
-		password=request.form["pass"],
-		address=request.form["address"],
-		phone=request.form["phone"]
-	)
+	data = request.get_json()
+	data["id"] = data.pop("email")
+	new_client = Client(**data)
 	ID = new_client.put()
-	# Make response "201 Created"
-	return make_response(jsonify({"created":ID}), 201)
+	return make_response(jsonify({"created":ID.urlsafe()}), 201)
 
-# getClients() - GET
 @app.route("/clients", methods=["GET"])
 def getClients():
-	return jsonify({"all":clients.values()})
+	clients = map(lambda c: c.json(), Client.query())
+	return make_response(jsonify({"all":clients}), 200)
 
-# deleteClient() - DELETE
 @app.route("/clients/<ID_client>", methods=["DELETE"])
 def deleteClient(ID_client):
-	return jsonify(clients.pop(ID_client))
+	retrieve(ID_client).delete()
+	return make_response(jsonify({"deleted":ID_client}), 200)
 
-# updateClient() - PUT
 @app.route("/clients/<ID_client>", methods=['PUT'])
 def updateClient(ID_client):
-	# Remove old email
-	old = clients.pop(ID_client)
-	# Rescue values from request and initialize carts to empty
-	new_client = {
-		"email":	request.form.get("email", old["email"]),
-		"pass":		request.form.get("pass", old["pass"]),
-		"address":	request.form.get("address", old["address"]),
-		"phone":	request.form.get("phone", old["phone"]),
-		"carts":	old['carts']
-	}
-	clients[new_client["email"]] = new_client
-	return make_response(jsonify({"updated":new_client['email']}), 200)
+	client = retrieve(ID_client).get()
+	data = request.get_json()
+	vals = ("address", "phone", "password")
+	for k,v in data.iteritems():
+		if k in vals: setattr(client, k, v)
+	client.put()
+	return make_response(jsonify({"updated":ID_client}), 200)
 
-# getClientDetails() - GET
 @app.route("/clients/<ID_client>", methods=["GET"])
 def getClientDetails(ID_client):
-	# Check whether client already exists
-	if not clients.has_key(ID_client):
-		abort(400) # Bad request
-	return jsonify(clients[ID_client])
+	client = retrieve(ID_client).get()
+	return make_response(jsonify({"client":client.json()}), 200)
 
 
-# addWine() - POST
 @app.route("/wines", methods=["POST"])
+@check_vals(["name", "type"])
 def addWine():
-	form = request.form
-	# Check whether values are present in the request
-	check_vals(["name", "type"])
-	# Generate universal ID (random)
-	ID = str(uuid4())
-	# Needed values
-	new_wine = {
-		'id': ID,
-		'grade': form.get("grade", "12"),
-		'size': form.get('size', "75"),
-		'varietals': form.getlist('varietals'),
-		'do': form.get("do", "0"),
-		'price': form.get("price", ""),
-		'name': form['name'],
-		'photo': form.get("photo", "")
-	}
-	# Decide depending on the type of wine
-	group = form['type']
-	if group == "red":
-		check_vals(["cask","bottle"])
-		new_wine.update({
-			"cask": form['cask'],
-			"bottle": form["bottle"]
-		})
-		grp_wines["red"][ID] = new_wine
-	elif group == "white":
-		grp_wines["white"][ID] = new_wine
-	elif group == "rose":
-		grp_wines["rose"][ID] = new_wine
-	else:
-		abort(400) # Bad request
-	all_wines[ID] = new_wine
-	# Make response "201 Created"
-	return make_response(jsonify({"created":ID}), 201)
+	data = request.get_json()
+	data["parent"] = retrieve(data.pop("type"), WineType)
+	new_wine = Wine(**data)
+	ID = new_wine.put()
+	return make_response(jsonify({"created":ID.urlsafe()}), 201)
 
-# allWines() - GET
 @app.route("/wines", methods=["GET"])
 def allWines():
-	return jsonify({"all":all_wines.values()})
+	wines = map(lambda w: w.json(), Wine.query())
+	return make_response(jsonify({"all":wines}), 200)
 
-# getWineProperties() - GET
 @app.route("/wines/<ID_wine>", methods=["GET"])
 def getWineProperties(ID_wine):
-	if not all_wines.has_key(ID_wine):
-		abort(404) # Not found
-	return jsonify(all_wines[ID_wine])
+	wine = retrieve(ID_wine).get()
+	return make_response(jsonify({"wine":wine.json()}), 200)
 
-# updateWine() - PUT
 @app.route("/wines/<ID_wine>", methods=["PUT"])
 def updateWine(ID_wine):
-	form = request.form
-	# Check whether wine is alredy inside 
-	if not all_wines.has_key(ID_wine):
-		abort(404) # Not found
-	old = all_wines[ID_wine]
-	# All regular values
-	new = {
-		'id': old['id'],
-		'grade': form.get("grade", old["grade"]),
-		'size': form.get('size', old["size"]),
-		'varietals': form.getlist('varietals') or old['varietals'],
-		'do': form.get("do", old["do"]),
-		'price': form.get("price", old["price"]),
-		'name': form.get('name', old['name']),
-		'photo': form.get("photo", old["photo"])
-	}
-	# Red wine values
-	if grp_wines['red'].has_key(ID_wine):
-		for v in red_wine_vals:
-			if request.form.has_key(v):
-				wine[v] = request.form[v]
+	data = request.get_json()
+	wine = retrieve(ID_wine).get()
+	vals = ("do", "grade", "size", "varietals", "price", "name", 
+		"photo", "cask", "bottle")
+	for k,v in data.iteritems():
+		if k in vals: setattr(wine, k, v)
+	wine.put()
 	return make_response(jsonify({"updated":ID_wine}), 200)
 
-# deleteWines() - DELETE
 @app.route("/wines", methods=["DELETE"])
 def deleteWines():
-	wines = all_wines.keys()
-	all_wines.clear()
-	for k,v in grp_wines.iteritems(): v.clear()
-	return make_response(jsonify({"deleted":wines}), 200)
+	wines = Wine.query().fetch(keys_only=True)
+	for w in wines: w.delete()
+	return make_response(jsonify({"deleted":"all"}), 200)
 
-# deleteWine() - DELETE
 @app.route("/wines/<ID_wine>", methods=["DELETE"])
 def deleteWine(ID_wine):
-	# Check whether the wine exists
-	if not all_wines.has_key(ID_wine):
-		abort(404) # Not found
-	# Removing
-	all_wines.pop(ID_wine)
-	for k,v in grp_wines.iteritems():
-		if v.has_key(ID_wine): 
-			v.pop(ID_wine)
-			break
-	# Make response 200 "OK"
+	wine = retrieve(ID_wine)
+	wine.delete()
 	return make_response(jsonify({"deleted":ID_wine}), 200)
 
-# wineByType() - GET
 @app.route("/types/<ID_type>", methods=["GET"])
 def wineByType(ID_type):
-	# Check whether the type exists
-	if not grp_wines.has_key(ID_type):
-		abort(404) # Not found
+	wines = Wine.query(ancestor=retrieve(ID_type, WineType))
+	wines = map(lambda w: w.json(), wines)
+	return make_response(jsonify({'all':wines}), 200)
 
-	results = grp_wines[ID_type]
-	return jsonify({'all':results.values()})
-
-# addCart() - POST
 @app.route("/clients/<ID_client>/carts", methods=["POST"])
 def addCart(ID_client):
-	# Generate universal ID (random)
-	ID = str(uuid4()) 
-	new_cart = {
-		"id":ID,
-		"name": request.form.get("name", ""),
-		"items":[]
-	}
-	# Update everything
-	carts[ID] = new_cart
-	clients[ID_client]['carts'].append(ID)
-	# Make response "201 Created"
-	return make_response(jsonify({"created":ID}), 201)
+	data = request.get_json()
+	data["parent"] = retrieve(ID_client)
+	new_cart = Cart(**data)
+	ID = new_cart.put()	
+	return make_response(jsonify({"created":ID.urlsafe()}), 201)
 
-# deleteCart() - DELETE
 @app.route("/clients/<ID_client>/carts/<ID_cart>", methods=["DELETE"])
-def deleteCart(ID_client, ID_cart):
-	# Check whether the cart and the client exist
-	if not (clients.has_key(ID_client) and clients[ID_client]['carts'].count(ID_cart)):
-		abort(404) # Not found
-	# Remove itself, also from client
-	carts.pop(ID_cart)
-	clients[ID_client]['carts'].remove(ID_cart)
-	# Make response "200 OK"
-	return make_response(jsonify({"deleted":ID_cart}), 200)
+@check_cart
+def deleteCart(key_client, key_cart):
+	key_cart.delete()
+	return make_response(jsonify({"deleted":key_cart.urlsafe()}), 200)
 
-# addItem() - POST
 @app.route("/clients/<ID_client>/carts/<ID_cart>/items", methods=["POST"])
-def addItem(ID_client, ID_cart):
-	# Check whether the cart and the client exist
-	if not (clients.has_key(ID_client) and clients[ID_client]['carts'].count(ID_cart)):
-		abort(404) # Not found
-	# Check if the value is present in the request and exists
-	check_vals(['item'])
-	item = request.form['item']
-	if not all_wines.has_key(item):
-		abort(404)
-	carts[ID_cart]['items'].append(item)
-	# Make response "201 Created"
-	return make_response(jsonify(carts[ID_cart]), 201)
+@check_vals(["item"])
+@check_cart
+def addItem(key_client, key_cart):
+	cart = key_cart.get()
+	key_wine = retrieve(request.get_json()['item'])
+	cart.items.append(key_wine)
+	cart.put()
+	return make_response(jsonify({"added":key_wine.urlsafe()}), 201)
 
-
-# getItems() - GET
 @app.route("/clients/<ID_client>/carts/<ID_cart>/items", methods=["GET"])
-def getItems(ID_client, ID_cart):
-	# Check whether the cart and the client exist
-	if not (clients.has_key(ID_client) and clients[ID_client]['carts'].count(ID_cart)):
-		abort(404) # Not found
-	# Make response "200 OK"
-	return make_response(jsonify({"all":carts[ID_cart]['items']}), 200)
-	
+@check_cart
+def getItems(key_client, key_cart):
+	wines = map(lambda w: w.get().json(), key_cart.get().items)
+	return make_response(jsonify({"all":wines}), 200)
 
-# delItem() - DELETE
 @app.route("/clients/<ID_client>/carts/<ID_cart>/items/<ID_wine>", methods=["DELETE"])
-def delItem(ID_client, ID_cart, ID_wine):
-	# Check whether the cart and the client exist
-	if not (clients.has_key(ID_client) and clients[ID_client]['carts'].count(ID_cart)
-		and all_wines.has_key(ID_wine)):
-		abort(404) # Not found
-	carts[ID_cart]['items'].remove(ID_wine)
-	# Make response "200 OK"
-	return make_response(jsonify({"deleted":ID_wine}), 200)
+@check_cart
+def delItem(key_client, key_cart, ID_wine):
+	cart = key_cart.get()
+	key_wine = dropwhile(lambda k: k.urlsafe()!=ID_wine, cart.items).next()
+	cart.items.remove(key_wine)
+	cart.put()
+	return make_response(jsonify({"removed":ID_wine}), 200)
 
-
-# updateItem() - PUT
 @app.route("/clients/<ID_client>/carts/<ID_cart>/items/<ID_wine>", methods=["PUT"])
-def updateItem(ID_client, ID_cart, ID_wine):
-	# Check whether the cart and the client exist
-	if not (clients.has_key(ID_client) and clients[ID_client]['carts'].count(ID_cart)):
-		abort(404) # Not found
-	check_vals(["item"])
-	if not all_wines.has_key(request.form['item']):
-		abort(404)
-	carts[ID_cart]['items'].remove(ID_wine)
-	carts[ID_cart]['items'].append(request.form["item"])
-	# Make response "200 OK"
-	return make_response(jsonify({"updated":request.form["item"]}), 200)
-
+@check_vals(["item"])
+@check_cart
+def updateItem(key_client, key_cart, ID_wine):
+	cart = key_cart.get()
+	key_wine = dropwhile(lambda k: k.urlsafe()!=ID_wine, cart.items).next()
+	cart.items.remove(key_wine)
+	item = retrieve(request.get_json()['item'])
+	cart.items.append(item)
+	cart.put()
+	return make_response(jsonify({"updated":item.urlsafe()}), 200)
 
 if __name__ == "__main__":
 	app.run()
